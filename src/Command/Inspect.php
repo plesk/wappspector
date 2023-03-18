61e5c5c96afd935d1ee76f3399d1c3743643ea33
Eugene Kazakov <ekazakov@plesk.com>
2023-03-18 13:00:36 +0100

    PhpMatcher implementation

--- src/Command/Inspect.php
+++ src/Command/Inspect.php
@@ -12,6 +12,16 @@ class Inspect extends Command
 {
     public function execute(InputInterface $input, OutputInterface $output): int
     {
+        $path = getcwd();
+        $adapter = new \League\Flysystem\Local\LocalFilesystemAdapter($path);
+        $fs = new \League\Flysystem\Filesystem($adapter);
+        $matcher = new \Plesk\Wappspector\WappMatchers\PhpMatcher($fs);
+        $results = $matcher->match('/');
+        $results = [...$results, ...$matcher->match('/')];
+        foreach ($results as $match) {
+            $output->writeln(json_encode($match, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR));
+        }
+
         return Command::SUCCESS;
     }
 }
--- src/WappMatchers/PhpMatcher.php
+++ src/WappMatchers/PhpMatcher.php
@@ -3,16 +3,34 @@
 namespace Plesk\Wappspector\WappMatchers;
 
 use League\Flysystem\Filesystem;
+use League\Flysystem\StorageAttributes;
 
 class PhpMatcher implements WappMatcherInterface
 {
-    public function __construct(private Filesystem $fs)
+    public function __construct(private readonly Filesystem $fs)
     {
-
     }
 
     public function match(string $path): iterable
     {
-        // TODO: Implement match() method.
+        $result = [];
+        $list = $this->fs->listContents($path);
+        foreach ($list as $item) {
+            /** @var StorageAttributes $item */
+            if ($item->isFile() && str_ends_with($item->path(), '.php')) {
+                $result[] = [
+                    'matcher' => 'php',
+                    'path' => $path,
+                ];
+                break;
+            }
+            if ($item->isDir() && $item->path() === 'src') {
+                $result = [
+                    ...$result,
+                    ...$this->match(rtrim($path, '/') . '/src'),
+                ];
+            }
+        }
+        return $result;
     }
 }
