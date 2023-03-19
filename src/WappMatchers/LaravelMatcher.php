0da18ac5c220e4a8c1c7b9fa16ff554eeca55fbd
Alexander Satretdinov <asatretdinov@plesk.com>
2023-03-19 13:06:06 +0100

    fix laravel matcher

--- src/WappMatchers/LaravelMatcher.php
+++ src/WappMatchers/LaravelMatcher.php
@@ -15,31 +15,30 @@ class LaravelMatcher implements WappMatcherInterface
      */
     public function match(Filesystem $fs, string $path): iterable
     {
+        $result = $this->getLaravelVersion($fs, $path);
+        if (!$result) {
+            return $this->getLaravelVersion($fs, rtrim($path) . '/../');
+        }
+
+        return $result;
+    }
+
+    /**
+     * @throws FilesystemException
+     */
+    private function getLaravelVersion(Filesystem $fs, string $path): ?array
+    {
         if (!$fs->fileExists(rtrim($path, '/') . '/' . self::ARTISAN)) {
             return [];
         }
 
-        $laravelPackage = $this->getLaravelVersion($fs, $path);
-        if ($laravelPackage) {
-            return [
-                'matcher' => 'laravel',
-                'path' => $path,
-                'version' => $laravelPackage,
-            ];
-        }
-
-        return [];
-    }
-
-    private function getLaravelVersion(Filesystem $fs, string $path): ?string
-    {
         $composerJson = json_decode($fs->read(rtrim($path, '/') . '/' . self::COMPOSER_JSON), JSON_FORCE_OBJECT | JSON_THROW_ON_ERROR);
         $laravelPackage = $composerJson['require']['laravel/framework'] ?? null;
 
-        if ($laravelPackage !== null) {
-            return str_replace('^', '', $laravelPackage);
-        }
-
-        return null;
+        return [
+            'matcher' => 'laravel',
+            'path' => $path,
+            'version' => $laravelPackage !== null ? str_replace('^', '', $laravelPackage) : null,
+        ];
     }
 }
