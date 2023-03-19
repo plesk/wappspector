1c83f582c3b2d69a5e6baa62fdba0d6be740b8e4
Eugene Kazakov <ekazakov@plesk.com>
2023-03-19 17:11:23 +0100

    Introduce CLI options for recursive directory traverse

--- src/Command/Inspect.php
+++ src/Command/Inspect.php
@@ -5,7 +5,9 @@ namespace Plesk\Wappspector\Command;
 use Plesk\Wappspector\Wappspector;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
 use Throwable;
@@ -16,14 +18,20 @@ class Inspect extends Command
     public function __construct(private Wappspector $wappspector)
     {
         parent::__construct();
+        $this->addOption('path', '', InputOption::VALUE_OPTIONAL, 'Root path', getcwd());
+        $this->addOption('recursive', '', InputOption::VALUE_NEGATABLE, 'Traverse directories recursive', true);
+        $this->addOption('depth', '', InputOption::VALUE_OPTIONAL, 'Depth of recurse', 1);
     }
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
         $logger = new ConsoleLogger($output);
+        $result = [];
 
         try {
-            $result = $this->wappspector->run(getcwd());
+            foreach ($this->getPath($input) as $path) {
+                $result = [...$result, ...$this->wappspector->run($path)];
+            }
 
             $output->writeln(json_encode($result, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR));
 
@@ -31,6 +39,34 @@ class Inspect extends Command
         } catch (Throwable $exception) {
             $logger->error($exception->getMessage());
             return Command::FAILURE;
+        }
+    }
+
+    private function getPath(InputInterface $input): iterable
+    {
+        $path = $input->getOption('path');
+        if (!$input->getOption('recursive')) {
+            yield $path;
+            return;
+        }
+
+        $flags = \FilesystemIterator::FOLLOW_SYMLINKS
+            | \FilesystemIterator::KEY_AS_PATHNAME
+            | \FilesystemIterator::CURRENT_AS_FILEINFO
+            | \FilesystemIterator::SKIP_DOTS;
+        $itFlags = \RecursiveIteratorIterator::SELF_FIRST;
+        $it = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path, $flags), $itFlags);
+        $it->setMaxDepth((int)$input->getOption('depth'));
+
+        foreach ($it as $path => $item) {
+            /** @var \SplFileInfo $item */
+            if (str_contains($path, '/.')) {
+                continue;
+            }
+            if (!$item->isDir()) {
+                continue;
+            }
+            yield $path;
         }
     }
 }
