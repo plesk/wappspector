bd82be8ec2bc57818038cd85d26dca90a23564af
Alexander Satretdinov <asatretdinov@plesk.com>
2023-03-19 10:46:11 +0100

    fix laravel matcher

--- src/WappMatchers/LaravelMatcher.php
+++ src/WappMatchers/LaravelMatcher.php
@@ -8,17 +8,18 @@ use League\Flysystem\FilesystemException;
 class LaravelMatcher implements WappMatcherInterface
 {
     private const COMPOSER_JSON = 'composer.json';
+    private const ARTISAN = 'artisan';
 
     /**
      * @throws FilesystemException
      */
     public function match(Filesystem $fs, string $path): iterable
     {
-        if (!$fs->fileExists(self::COMPOSER_JSON)) {
+        if (!$fs->fileExists(self::ARTISAN)) {
             return [];
         }
 
-        $laravelPackage = $this->getLaravelPackage($fs);
+        $laravelPackage = $this->getLaravelVersion($fs);
         if ($laravelPackage) {
             return [
                 'matcher' => 'laravel',
@@ -30,7 +31,7 @@ class LaravelMatcher implements WappMatcherInterface
         return [];
     }
 
-    private function getLaravelPackage(Filesystem $fs): ?string
+    private function getLaravelVersion(Filesystem $fs): ?string
     {
         $composerJson = json_decode($fs->read(self::COMPOSER_JSON), JSON_FORCE_OBJECT | JSON_THROW_ON_ERROR);
         $laravelPackage = $composerJson['require']['laravel/framework'] ?? null;
