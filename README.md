72bf22dee9bff496502702114029147621328d93
Anton Siuvaev <asiuvaev@plesk.com>
2023-07-17 10:14:55 +0300

    add possibility to change order of matchers

--- README.md
+++ README.md
@@ -26,3 +26,20 @@ Command-line interface utility to analyze the file structure of a web hosting se
 * php -d phar.readonly=off ~/.composer/vendor/bin/phar-composer build .
 * ./wappspector.phar
 
+## Changing matchers order
+To change the matchers order or to disable some of them, you should override `matchers` entry of DI container.
+
+```php
+$diContainer = \Plesk\Wappspector\DIContainer::build();
+$matchers = $diContainer->get('matchers');
+array_unshift($matchers, \Some\New\Matcher::class);
+$diContainer->set('matchers', $matchers);
+```
+
+or
+
+```php
+// only detect WordPress installs
+$diContainer = \Plesk\Wappspector\DIContainer::build();
+$diContainer->set('matchers', [\Plesk\Wappspector\WappMatchers\WordpressMatcher::class]);
+```
--- src/container.php
+++ src/container.php
@@ -9,23 +9,30 @@ use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application;
 
 return [
-    Wappspector::class => static function (Container $container): Wappspector {
-        return new Wappspector($container->get(FileSystemFactory::class), [
-            $container->get(WappMatchers\JoomlaMatcher::class),
-            $container->get(WappMatchers\WordpressMatcher::class),
-            $container->get(WappMatchers\DrupalMatcher::class),
-            $container->get(WappMatchers\PrestashopMatcher::class),
-            $container->get(WappMatchers\Typo3Matcher::class),
-            $container->get(WappMatchers\LaravelMatcher::class),
-            $container->get(WappMatchers\DotNetMatcher::class),
-            $container->get(WappMatchers\RubyMatcher::class),
-            $container->get(WappMatchers\PythonMatcher::class),
-            $container->get(WappMatchers\NodeJsMatcher::class),
+    'matchers' => [
+        WappMatchers\JoomlaMatcher::class,
+        WappMatchers\WordpressMatcher::class,
+        WappMatchers\DrupalMatcher::class,
+        WappMatchers\PrestashopMatcher::class,
+        WappMatchers\Typo3Matcher::class,
+        WappMatchers\LaravelMatcher::class,
+        WappMatchers\DotNetMatcher::class,
+        WappMatchers\RubyMatcher::class,
+        WappMatchers\PythonMatcher::class,
+        WappMatchers\NodeJsMatcher::class,
 
-            // Low priority wrappers. Should go last.
-            $container->get(WappMatchers\ComposerMatcher::class),
-            $container->get(WappMatchers\PhpMatcher::class),
-        ]);
+        // Low priority wrappers. Should go last.
+        WappMatchers\ComposerMatcher::class,
+        WappMatchers\PhpMatcher::class,
+    ],
+    Wappspector::class => static function (Container $container): Wappspector {
+        $matchers = [];
+
+        foreach ($container->get('matchers') as $matcher) {
+            $matchers[] = $container->get($matcher);
+        }
+
+        return new Wappspector($container->get(FileSystemFactory::class), $matchers);
     },
     Inspect::class => static function (ContainerInterface $container): Inspect {
         return new Inspect($container->get(Wappspector::class));
