b1a710c5927b65b9790971a3d3a862f22e230242
Anton Syuvaev <asiuvaev@plesk.com>
2023-09-28 15:38:27 +0300

    Catch exceptions for inaccessible and non-existent dirs (#22)

--- src/WappMatchers/PhpMatcher.php
+++ src/WappMatchers/PhpMatcher.php
@@ -5,6 +5,7 @@ namespace Plesk\Wappspector\WappMatchers;
 use League\Flysystem\Filesystem;
 use League\Flysystem\FilesystemException;
 use League\Flysystem\StorageAttributes;
+use League\Flysystem\UnableToListContents;
 use Plesk\Wappspector\Matchers;
 
 class PhpMatcher implements WappMatcherInterface
@@ -14,20 +15,24 @@ class PhpMatcher implements WappMatcherInterface
      */
     public function match(Filesystem $fs, string $path): iterable
     {
-        $list = $fs->listContents($path);
-        foreach ($list as $item) {
-            /** @var StorageAttributes $item */
-            if ($item->isFile() && str_ends_with($item->path(), '.php')) {
-                return [
-                    'matcher' => Matchers::PHP,
-                    'path' => $path,
-                    'version' => null,
-                ];
-            }
+        try {
+            $list = $fs->listContents($path);
+            foreach ($list as $item) {
+                /** @var StorageAttributes $item */
+                if ($item->isFile() && str_ends_with($item->path(), '.php')) {
+                    return [
+                        'matcher' => Matchers::PHP,
+                        'path' => $path,
+                        'version' => null,
+                    ];
+                }
 
-            if ($item->isDir() && $item->path() === ltrim(rtrim($path, '/') . '/src', '/')) {
-                return $this->match($fs, rtrim($path, '/') . '/src');
+                if ($item->isDir() && $item->path() === ltrim(rtrim($path, '/') . '/src', '/')) {
+                    return $this->match($fs, rtrim($path, '/') . '/src');
+                }
             }
+        } catch (UnableToListContents) {
+            // skip dir if it is inaccessible
         }
 
         return [];
