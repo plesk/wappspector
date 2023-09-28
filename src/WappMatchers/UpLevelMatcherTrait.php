842881ca5d3128400e8f603a6df2401279c35a9d
Anton Siuvaev <anton.siuvaev@webpros.com>
2023-09-28 14:10:13 +0300

    Catch exceptions for inaccessible and non-existent dirs

--- src/WappMatchers/UpLevelMatcherTrait.php
+++ src/WappMatchers/UpLevelMatcherTrait.php
@@ -3,24 +3,24 @@
 namespace Plesk\Wappspector\WappMatchers;
 
 use League\Flysystem\Filesystem;
-use League\Flysystem\FilesystemException;
 use League\Flysystem\UnableToListContents;
 
 trait UpLevelMatcherTrait
 {
     abstract protected function doMatch(Filesystem $fs, string $path): array;
 
-    /**
-     * @throws FilesystemException
-     */
     public function match(Filesystem $fs, string $path): array
     {
-        if (!$result = $this->doMatch($fs, $path)) {
-            try {
-                $result = $this->doMatch($fs, rtrim($path) . '/../');
-            } catch (UnableToListContents $e) {
-                // skip parent dir if it is inaccessible
-            }
+        return $this->safeScanDir($fs, $path) ?: $this->safeScanDir($fs, rtrim($path) . '/../');
+    }
+
+    private function safeScanDir(Filesystem $fs, string $path): array
+    {
+        $result = [];
+        try {
+            $result = $this->doMatch($fs, $path);
+        } catch (UnableToListContents) {
+            // skip dir if it is inaccessible
         }
 
         return $result;
